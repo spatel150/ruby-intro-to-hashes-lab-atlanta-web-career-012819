dd612552e0630f888725020a48caf26b9c73e6c4
Samidh Patel <spatel13456@gmail.com>
2019-01-15 04:59:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,12 +16,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {
-	  :railroads => {
-	    :pieces => 4,
-	    :names => {},
-	    :rent_in_dollars => {}
-	  }
+	monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}
 	}
   return monopoly_with_second_tier
 end
